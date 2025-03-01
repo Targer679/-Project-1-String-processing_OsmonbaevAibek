ea6cf385d7db3f3c58b145d686cf096abd5fc299
Aibek <aibekosmonbaev2025@proton.me>
2025-03-01 13:48:35 +0600

    UPDATE2

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,15 @@ public class Main {
         // Create an array of StringBuilder to store the words
         StringBuilder[] words = new StringBuilder[20];
         int index = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("WELCOME TO THE HANGMAN GAME!!\n" +
+                "THE GOAL OF THIS GAME IS TO GUESS THE WORD BY TYPING LETTERS\n" +
+                "YOU HAVE 8 LIVES, EACH WRONG GUESS WILL TAKE AWAY 1 LIVE\n"+
+                "IF YOU HAVE 0 LIVES, THE GAME WILL END\n" +
+                "TYPE IN CAPITAL LETTERS ONLY" );
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\ajbek\\IdeaProjects\\Project-1_String_processing\\out\\production\\Project-1_String_processing\\words.txt"));
+            BufferedReader reader = new BufferedReader(new FileReader("src/words.txt"));
             String line;
 
             // Read the file line by line
@@ -23,14 +29,15 @@ public class Main {
 
                 // Store each word in a StringBuilder object
                 for (String word : wordArray) {
-                    words[index] = new StringBuilder(word);
-                    index++;
+                    if (word != null && index < words.length) {
+                        words[index] = new StringBuilder(word);
+                        index++;
+                    }
                 }
+
             }
 
             reader.close();
-
-
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -39,10 +46,107 @@ public class Main {
         // Generate a random index
         Random random = new Random();
         int rand = random.nextInt(20);
-
+        int lv = 8;
         // Print underscores for each character in the selected word
-        for (int i = 0; i < words[rand].length(); i++) {
-            System.out.print("_");
+        StringBuilder guess = new StringBuilder(words[rand]);
+        StringBuilder mq = new StringBuilder(words[rand]);
+        String dq = mq.toString();
+        for (int i = 0; i < guess.length(); i++) {
+            guess.replace(i, i + 1, "_");
+        }
+        System.out.println(guess.length());
+        System.out.println(guess);
+
+        while (!guess.toString().equals(mq.toString())) {
+
+            char x = scanner.next().charAt(0);
+            if (dq.contains(String.valueOf(x))) {
+                guess.replace(dq.indexOf(String.valueOf(x)), dq.indexOf(String.valueOf(x)) + 1, String.valueOf(x));
+                guess.replace(dq.lastIndexOf(String.valueOf(x)), dq.lastIndexOf(String.valueOf(x)) + 1, String.valueOf(x));
+                System.out.println(guess);
+            }
+            if (!dq.contains(String.valueOf(x))) {
+                lv = lv - 1;
+                System.out.println("LIVES LEFT: " + lv);
+                if (lv == 7) {
+                    System.out.println("  *****\n" +
+                            "  *\n" +
+                            "  *\n" +
+                            "  *\n" +
+                            "  *\n" +
+                            "*****");
+                }
+                if (lv == 6) {
+                    System.out.println("  *****\n" +
+                            "  *   |\n" +
+                            "  *\n" +
+                            "  *\n" +
+                            "  *\n" +
+                            "*****");
+                }
+                if (lv == 5) {
+                    System.out.println("  *****\n" +
+                            "  *   |\n" +
+                            "  *   O\n" +
+                            "  *\n" +
+                            "  *\n" +
+                            "  *\n" +
+                            "*****");
+                }
+                if (lv == 4) {
+                    System.out.println("  *****\n" +
+                            "  *   |\n" +
+                            "  *   O\n" +
+                            "  *   |\n" +
+                            "  *\n" +
+                            "  *\n" +
+                            "*****");
+                }
+                if (lv == 3) {
+                    System.out.println("  *****\n" +
+                            "  *   |\n" +
+                            "  *   O\n" +
+                            "  *  /|\n" +
+                            "  *\n" +
+                            "  *\n" +
+                            "*****");
+                }
+                if (lv == 2) {
+                    System.out.println("  *****\n" +
+                            "  *   |\n" +
+                            "  *   O\n" +
+                            "  *  /|\\\n" +
+                            "  *\n" +
+                            "  *\n" +
+                            "*****");
+                }
+                if (lv == 1) {
+                    System.out.println("  *****\n" +
+                            "  *   |\n" +
+                            "  *   O\n" +
+                            "  *  /|\\\n" +
+                            "  *  /\n" +
+                            "  *\n" +
+                            "*****");
+                }
+                if (lv == 0) {
+                    System.out.println("  *****\n" +
+                            "  *   |\n" +
+                            "  *   O\n" +
+                            "  *  /|\\\n" +
+                            "  *  / \\\n" +
+                            "  *\n" +
+                            "*****");
+                    System.out.println("GAME OVER(");
+                    break;
+                }
+
+
+
+            }
+            if(guess.toString().equals(mq.toString())){
+                System.out.println("CONGRATULATIONS!!\n" + "YOU HAVE WON!!");
+            }
         }
     }
 }
